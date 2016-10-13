a07127266f50e1c4d45d3a10660b41c9c75df786
Mohammad Azim Khan <azim.khan@arm.com>
2016-10-13 11:57:50 +0100

    Pipeline steps implementation

---
+++ src/main/java/hudson/plugins/testlink/pipeline/TestLinkCreateBuildExecution.java
@@ -0,0 +1,95 @@
+package hudson.plugins.testlink.pipeline;
+
+import com.google.inject.Inject;
+import hudson.AbortException;
+import hudson.Launcher;
+import hudson.console.ConsoleNote;
+import hudson.model.*;
+import hudson.plugins.testlink.TestLinkBuilder;
+import hudson.plugins.testlink.TestLinkBuilderDescriptor;
+import hudson.plugins.testlink.TestLinkInstallation;
+import hudson.plugins.testlink.TestLinkSite;
+import hudson.plugins.testlink.result.ResultSeeker;
+import hudson.plugins.testlink.util.TestLinkHelper;
+import hudson.tasks.BuildStep;
+import org.apache.tools.ant.taskdefs.Parallel;
+import org.jenkinsci.plugins.workflow.steps.AbstractSynchronousNonBlockingStepExecution;
+import org.jenkinsci.plugins.workflow.steps.StepContextParameter;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by azikha01 on 11/10/2016.
+ */
+public class TestLinkCreateBuildExecution extends AbstractSynchronousNonBlockingStepExecution<java.lang.Void>{
+
+    @Inject
+    private transient TestLinkCreateBuildStep config;
+
+    @StepContextParameter
+    private transient TaskListener listener;
+
+    @StepContextParameter
+    private transient Launcher launcher;
+
+    @StepContextParameter
+    private transient Run<?, ?> build;
+
+    @Override
+    protected Void run() throws Exception{
+        listener.getLogger().println(config.getTestLinkName() + " \n" +
+                config.getTestProjectName() + " \n" +
+                config.getTestPlanName() + " \n" +
+                config.getTestPlanCustomFields() + " \n" +
+                config.getBuildName() + " \n" +
+                config.getBuildCustomFields() + " \n" +
+                config.getCustomFields() + " \n" +
+                config.getTestProjectName() + " \n"
+        );
+
+        TestLinkBuilderDescriptor DESCRIPTOR = new TestLinkBuilderDescriptor();
+        final TestLinkInstallation installation = DESCRIPTOR
+                .getInstallationByTestLinkName(config.getTestLinkName());
+        if (installation == null) {
+            throw new AbortException("Failed to find TestLink installation: " + config.getTestLinkName());
+        }
+        TestLinkHelper.setTestLinkJavaAPIProperties(installation.getTestLinkJavaAPIProperties(), listener);
+
+        final TestLinkSite testLinkSite;
+        final String testLinkUrl = installation.getUrl();
+        final String testLinkDevKey = installation.getDevKey();
+
+        TestLinkBuilder builder = new TestLinkBuilder(config.getTestLinkName(),
+                config.getTestProjectName(),
+                config.getTestPlanName(),
+                config.getPlatformName(),
+                config.getBuildName(),
+                config.getBuildCustomFields(),
+                config.getCustomFields(),
+                config.getTestPlanCustomFields(),
+                false,
+                false,
+                false,
+                false,
+                new ArrayList<BuildStep>(),
+                new ArrayList<BuildStep>(),
+                new ArrayList<BuildStep>(),
+                new ArrayList<BuildStep>(),
+                false, true, false, false, new ArrayList< ResultSeeker>()
+        );
+        builder.getTestLinkSite(testLinkUrl,
+                testLinkDevKey,
+                config.getTestProjectName(),
+                config.getTestPlanName(),
+                config.getPlatformName(),
+                config.getBuildName(),
+                config.getBuildCustomFields(),
+                config.getBuildNotes());
+
+        return null;
+    }
+}
